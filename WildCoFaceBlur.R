ced523f83f6ba223ef2daea155cba358e5035334
Fennell <mitchell.fennell@ubc.ca>
2021-01-14 19:20:01 -0800

    Added initial looping structure for files

--- WildCoFaceBlur.R
+++ WildCoFaceBlur.R
@@ -20,6 +20,8 @@ getwd() # Should be location of .Rproj, which should also contain your inputs, o
 json_in <- "CATH31_test.json"
 img_dir_in <- "CATH31_test"
 img_dir_out <- "CATH31_test_out_R"
+blur_level <- 7.0
+conf <- 0.3
 
 ### 2. Load in Python script ####
 py_install("opencv-python", pip = TRUE) #This may take a minute your first time 
@@ -27,4 +29,16 @@ source_python("FaceBlur.py")
 
 ### 3. Execute blurring ####
 face_blur(json_in,img_dir_in,img_dir_out)
+
+# Looooooooooop
+
+img_dirs <- list.dirs(img_dir_in) #contains imgs in site folders WITH relevant .JSON
+n.sites <- len(img_dirs)
+for (site in 1:len(img_dirs)){
+  json_in <- 
+  site_dir_out <- 
+  site_dir_in <- 
+  face_blur(json_in,site_dir_in,site_dir_out,blur_level,conf)
+  print(paste0("Site",site,"of",len(img_dirs),"complete"))
+}
 
